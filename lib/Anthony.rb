47416dac6e5127a9a279e4a608988c91ece52a12
Ryan Hardin <rhardin378@gmail.com>
2021-02-24 04:09:05 +0000

    Automatically backed up by Learn

--- lib/Anthony.rb
+++ lib/Anthony.rb
@@ -1,11 +1,6 @@
 def How_Big_A_Bitch_Ant_is
- Anthony = "Big Bitch"
-  puts Anthony
-  puts Anthony
-  puts Anthony
-  puts Anthony
+  Anthony = "Big Bitch"
   puts Anthony
   puts Anthony
 end
 
-puts How_Big_A_Bitch_Ant_is
